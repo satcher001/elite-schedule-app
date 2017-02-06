a8af126752b35e0aedc2c250b249912494c33414
A.J. Satcher <a.j.satcher@AJs-MacBook-Pro.local>
2017-02-06 12:04:53 -0500

    edited

--- src/pages/team-detail/team-detail.ts
+++ src/pages/team-detail/team-detail.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-//import { TeamsPage } from '../pages';
+import { MyTeamsPage } from '../pages';
 
 /*
   Generated class for the TeamDetail page.
@@ -13,6 +13,11 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'team-detail.html'
 })
 export class TeamDetailPage {
+
+  goHome(){
+             //this.navCtrl.push(MyTeamsPage);
+             this.navCtrl.parent.parent.popToRoot();
+    }
 
    team: any;
   constructor(public navCtrl: NavController, private navParams: NavParams) {
--- src/pages/team-home/team-home.ts
+++ src/pages/team-home/team-home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { StandingsPage } from '../pages';
-import { TeamDetailPage } from '../pages';
+import { TeamDetailPage, MyTeamsPage } from '../pages';
 
 /*
   Generated class for the TeamHome page.
@@ -9,19 +9,36 @@ import { TeamDetailPage } from '../pages';
   See http://ionicframework.com/docs/v2/components/#navigation for more info on
   Ionic pages and navigation.
 */
+
+
+
 @Component({
   selector: 'page-team-home',
   templateUrl: 'team-home.html'
 })
+
 export class TeamHomePage {
+
+  goHome(){
+             //this.navCtrl.push(MyTeamsPage);
+             this.navCtrl.popToRoot();
+             console.log('**parent', this.navCtrl.parent);
+             //this.navCtrl.parent.parent.popToRoot();
+      }
+
 
   teamDetailTab = TeamDetailPage;
   standingsTab = StandingsPage;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  team: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, ) {
+    this.team = this.navParams.data;
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamHomePage');
   }
+
+
 
 }
